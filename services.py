a1a60c1350359932616b49204d750e5a6df3b9f9
myoav <57358388+myoav@users.noreply.github.com>
2020-04-28 12:12:16 +0300

    almost done

--- services.py
+++ services.py
@@ -1,41 +1,38 @@
 import datetime
 import pymongo
 import json
-
-
 class Services:
-    # def __init__(self):
-    #     client = pymongo.MongoClient("mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority")
-    def insert_new_user(self, jso):
-        client = pymongo.MongoClient(
-            "mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority")
-        db = client.users
-        idd = jso['ID']
-        nickname = jso['Nickname']
-        username = jso['Username']
-        password = jso['Password']
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority")
+        self.db = self.client.users
+    def insert_new_user(self, jsonuser):
+        # client = pymongo.MongoClient(
+        #     "mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority")
+        # db = client.users
+        # check the argument
+        # if len(jsonuser['ID']) > 32 || len():
+        #     return "the ID to large"
         create_time = datetime.datetime.now()
-        status = jso['Status']
-        db.profiles.insert_one(
-            {'_id': idd,
-             "nickname": nickname,
-             "username": username,
-             "password": password,
-             "create_time": create_time,
-             "status": status}
+        self.db.profiles.insert_one(
+            {
+             '_id': jsonuser['ID'],
+             "Nick name": jsonuser['Nick name'],
+             "User name": jsonuser['User name'],
+             "password": jsonuser['Password'],
+             "Create time": create_time,
+             "Status": jsonuser['Status']
+             }
         )
-        # result = db.profiles.create_index([('id', pymongo.ASCENDING)],unique = True)
-        jso['create_time'] = create_time
-
-        if 'Password' in jso:
-            del jso['Password']
-        return jso
+        jsonuser['create_time'] = create_time
+        if 'Password' in jsonuser:
+            del jsonuser['Password']
+        return jsonuser
 
     def export_all_users(self):
-        client = pymongo.MongoClient(
-            "mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority")
-        db = client.users
+        # client = pymongo.MongoClient(
+        #     "mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority")
+        # db = client.users
         # for x in db.profiles.find({}, {"password": 0}):
         #     print(x)
 
-        return    list(db.profiles.find({}, {"password": 0}))
+        return list(self.db.profiles.find({}, {"password": 0}))
