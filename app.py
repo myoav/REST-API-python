1113eaa2f76531b4800b9baad3e86d6d550e064c
myoav <57358388+myoav@users.noreply.github.com>
2020-04-28 20:50:38 +0300

    Added handle errors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request, abort, make_response
 from services import Services
+
 app = Flask(__name__)
 services = Services()
 
@@ -8,13 +9,26 @@ services = Services()
 def hello_world():
     return 'yoyo'
 
+
 @app.route('/api/users', methods=['POST'])
 def new_user():
-    return  services.insert_new_user(request.json)
-    # return jsonify({'my data ':services.insert_new_user(request.json)})
+    return services.insert_new_user(request.json)
+
+
 @app.route('/api/admin/users', methods=['GET'])
 def ecport_user():
-    return  jsonify({'users':services.export_all_users()})
+    return jsonify({'users': services.export_all_users()})
+
+
+@app.errorhandler(404)
+def notFound(error):
+    return make_response("NOT FOUND", 404)
+
+
+@app.errorhandler(400)
+def notFound(error):
+    return make_response("INVALID INPUT", 400)
+
 
 if __name__ == '__main__':
     app.run()
--- services.py
+++ services.py
@@ -1,27 +1,33 @@
 import datetime
 import pymongo
 import json
+from flask import abort
+
+
 class Services:
     def __init__(self):
-        self.client = pymongo.MongoClient("mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority",connect=False)
+        self.client = pymongo.MongoClient(
+            "mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority", connect=False)
         self.db = self.client.users
+
     def insert_new_user(self, jsonuser):
-        # client = pymongo.MongoClient(
-        #     "mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority")
-        # db = client.users
         # check the argument
-        # if len(jsonuser['ID']) > 32 || len():
-        #     return "the ID to large"
+        if len(jsonuser['ID']) > 32 | len(jsonuser['Nickname']) > 11 | len(jsonuser['Username']) > 50 | len(
+                jsonuser['Password']) > 200 | jsonuser['Status'] > 255:
+            abort(400)
         create_time = datetime.datetime.now()
+        x = list(self.db.profiles.find({'_id': jsonuser['ID']}))
+        if x:
+            abort(400)
         self.db.profiles.insert_one(
             {
-             '_id': jsonuser['ID'],
-             "Nick name": jsonuser['Nick name'],
-             "User name": jsonuser['User name'],
-             "password": jsonuser['Password'],
-             "Create time": create_time,
-             "Status": jsonuser['Status']
-             }
+                '_id': jsonuser['ID'],
+                "Nickname": jsonuser['Nickname'],
+                "User name": jsonuser['User name'],
+                "password": jsonuser['Password'],
+                "Create time": create_time,
+                "Status": jsonuser['Status']
+            }
         )
         jsonuser['create_time'] = create_time
         if 'Password' in jsonuser:
@@ -29,10 +35,4 @@ class Services:
         return jsonuser
 
     def export_all_users(self):
-        # client = pymongo.MongoClient(
-        #     "mongodb+srv://Yoav:Yoav1993@cluster0-pzmee.mongodb.net/test?retryWrites=true&w=majority")
-        # db = client.users
-        # for x in db.profiles.find({}, {"password": 0}):
-        #     print(x)
-
         return list(self.db.profiles.find({}, {"password": 0}))
